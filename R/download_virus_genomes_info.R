ada6f372a5961fa2acd689abda4bfd0e87d324f2
Taavi Päll <tapa741@gmail.com>
2019-03-03 15:45:36 +0200

    tidy style

--- R/download_virus_genomes_info.R
+++ R/download_virus_genomes_info.R
@@ -11,13 +11,13 @@ library(stringr)
 
 viruses <- read_tsv("viruses.txt")
 
-viruses <- rename_all(viruses, str_replace_all, "^[[:punct:]]", "") %>% 
-    rename_all(str_to_lower) %>% 
-    rename_all(str_replace_all, "\\/", " ") %>%  
-    rename_all(str_replace_all, "[[:punct:]]", "") %>% 
-    rename_all(str_replace_all, "\\s+", "_") %>% 
-    rename_all(str_replace, "segmemts", "segments") %>% 
-    rename(tax_id = taxid)
+viruses <- rename_all(viruses, str_replace_all, "^[[:punct:]]", "") %>%
+  rename_all(str_to_lower) %>%
+  rename_all(str_replace_all, "\\/", " ") %>%
+  rename_all(str_replace_all, "[[:punct:]]", "") %>%
+  rename_all(str_replace_all, "\\s+", "_") %>%
+  rename_all(str_replace, "segmemts", "segments") %>%
+  rename(tax_id = taxid)
 
 write_csv(viruses, path = "viruses.csv")
 file.remove("viruses.txt")
